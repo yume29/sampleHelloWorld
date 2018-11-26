9c6e3fb1e3077aa8e6feb91e25bbb891d3089566
一ノ瀬由芽 <srv123123@me.com>
2018-11-26 13:37:58 +0800

    Initial Commit

---
+++ sampleHalloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleHalloWorld
+//
+//  Created by 一ノ瀬由芽 on 2018/11/26.
+//  Copyright © 2018年 Yume Ichinose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
