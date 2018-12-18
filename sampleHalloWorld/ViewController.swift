708bcd140cd10d9a042abd144da1bcc972a688c8
一ノ瀬由芽 <srv123123@me.com>
2018-12-18 14:25:20 +0800

    コミットしまーす

--- sampleHalloWorld/ViewController.swift
+++ sampleHalloWorld/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+//日本語変換ボタンが押された時
+    @IBAction func tapBtnJa(_ sender: Any) {
+        
+//        myLabelに書かれている文字を「こんにちは、世界」という日本語に変更したい
+        myLabel.text = "日本語：" + "こんにちは、世界"
+    }
+    
+    @IBAction func tapBtnEn(_ sender: Any) {
+//        英語に戻す
+        myLabel.text = "Hello, world"
+    }
+    
+    @IBAction func changeSwitch(_ sender: UISwitch) {
+//        スイッチオンオフ
+        print(sender.isOn)
+        
+        var lang = sender.isOn
+        if lang == true{
+            myLabel.text = "Hello, world"
+        }else{
+            myLabel.text = "こんにちは、世界"
+        }
+    }
+    
+    @IBAction func slider(_ sender: Any) {
+    }
+    
+    
+    @IBAction func textFlied(_ sender: Any) {
+    }
 }
 
